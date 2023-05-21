d41d5210d0b777ef43d3914f59274f2dd2591155
Nawar T <nawarturk11@gmail.com>
2023-05-21 18:58:17 -0400

    refactoring searchImages()

--- src/api.js
+++ src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (searchTerm) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID 5s6SfrObtbErhcjFMEMQ4xMizWMXh8qciH_VywNwh9E'
         },
         params: {   
-            query: 'forests'
-        }
+            query: searchTerm,
+        },
     });
 
-    console.log(response);
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;
