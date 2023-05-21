f260f28186d0e4d0a69a82084677824cfcf6cbad
Nawar T <nawarturk11@gmail.com>
2023-05-21 17:48:10 -0400

    setting up the project

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const searchImages = async(term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID 5s6SfrObtbErhcjFMEMQ4xMizWMXh8qciH_VywNwh9E'
+        },
+        params: {
+            query: term,
+        },
+    });
+    return response.data.results;
+};
+
+export default searchImages;
