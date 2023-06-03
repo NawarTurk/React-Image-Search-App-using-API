c91e8472e09a008707322e9125506373947c4d50
Nawar T <nawarturk11@gmail.com>
2023-06-03 17:43:04 -0400

    handlign the input element in the search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,24 @@
+import {useState} from 'react';
+
 function SearchBar({onSubmit}) {
-  const handleClick = () => {
+const [term, setTerm] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
     onSubmit('cars');
-  };  
+    
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
   return (
     <div>
-      <input/>
-      <button onClick = {handleClick}>click me</button>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange = {handleChange}/>
+      </form>
     </div>
     );
 }
